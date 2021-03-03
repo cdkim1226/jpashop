5dd2c2bb46e8eafe83643df3103e00a1542237ee
김창대 <changdae.kim@timf.co.kr>
2021-03-04 04:38:01 +0900

    Add Hello.html & HelloController

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
+
 	public static void main(String[] args) {
 
 		Hello hello = new Hello();
