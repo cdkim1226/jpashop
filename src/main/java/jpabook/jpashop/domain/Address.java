d2c0cd6ce6dc60d05d4ff780b0ed567abe8742b2
김창대 <cdkim1226@gmail.com>
2021-03-05 02:35:28 +0900

    주석 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -16,7 +16,7 @@ public class Address {
 
     }
 
-    public Address(String city, String street, String zipcode) {
+    public Address(String city, String street, String zipcode) { // setter 대신 사용 할 수 있는 객체를 생성해줌
         this.city = city;
         this.street = street;
         this.zipcode = zipcode;
