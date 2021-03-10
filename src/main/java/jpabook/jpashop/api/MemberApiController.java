a5aa0941e57ae912b9550c537093538bb9938b7e
김창대 <cdkim1226@gmail.com>
2021-03-11 05:49:46 +0900

    Create Member SelectList version V1

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.function.LongBinaryOperator;
 
 
@@ -17,8 +18,14 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
